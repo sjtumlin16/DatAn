4cbe84f22c960263d536ad8251781e6ebf44bd9e
Seth Tumlin <sjtumlin16@fuse.net>
2020-01-17 23:01:57 -0500

    Multi per line printing is now functional.

--- Histogram.py
+++ Histogram.py
@@ -10,7 +10,9 @@ fid.close()
 for i in range(len(x_list)):
     x_list[i] = float(x_list[i])
     
-Bin = [0, 0, 0, 0, 0, 0, 0]
+num = 7
+maxVal = 0
+Bin = [0]*num
 
 #Analysis
 n = len(x_list)
@@ -25,28 +27,35 @@ for k in range(n):
         Bin[3] = Bin[3] + 1
     elif x_list[k] < 1.030:
         Bin[4] = Bin[4] + 1
-    elif x_list[k] <= 1.038:
+    elif x_list[k] <= 1.034:
         Bin[5] = Bin[5] + 1
+    elif x_list[k] <= 1.038:
+        Bin[6] = Bin[6] + 1
 
 row = [0]*8
 
-for k in range(6):
+for k in range(num):
     b = k + 1
     print('Bin {0} = {1}'.format(b,Bin[k]))
+    if Bin[k] > maxVal:
+        maxVal = Bin[k]
 
 print(" \n")
 
-for i in range(9, -1, -1):
+for i in range(maxVal, -1, -1):
     row[0] = i + 1
-    for k in range(6):
+    for k in range(num):
         if Bin[k] >= i + 1:
             row[k+1] = "|//|"
         elif  Bin[k] == i:
             row[k+1] = " __ "
         else:
             row[k+1] = "    "
-    time.sleep(0.05)
-    if i == 9:
+    if row[0] >= 10:
         row[2] = "   "
-    print("{0}  {1}{2}{3}{4}{5}{6}".format(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+    
+    for j in range(num + 1):
+        print("{0}".format(row[j]), end="", flush=True)
+        time.sleep(0.015)
+    print("\n", end="", flush=True)
 print("\n")
